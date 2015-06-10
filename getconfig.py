299ecc6927a84e0b28b0939b343f5a211be82291
Alexander Dupuy <alex.dupuy@mac.com>
2015-06-10 12:57:42 +0200

    getconfig.py key/value sanity enforcement
    mangle keys for shell identifier compatibility
    add shell-compatible quoting for values

--- getconfig.py
+++ getconfig.py
@@ -13,10 +13,16 @@ import os
 import subprocess
 import sys
 import tempfile
+import warnings
 
 # '2013-10-17' is pinned config drive API revision, 'latest' might work as well
 META_NAME = str(os.path.join('openstack', '2013-10-17', 'meta_data.json'))
 CONFIG_DRIVE = '/dev/disk/by-label/config-2'
+
+# translation table bytestring for mapping illegal metadata key chars to '_'
+LEGALIZE = b''.join(
+    [chr(b).encode() if chr(b).isalnum() else b'_' for b in range(128)]
+) + b'_' * 128
 
 
 def metadata(meta_name=None):
@@ -66,8 +72,39 @@ def metadata(meta_name=None):
             pass
 
 
+def outformat(key, val):
+    r"""Format a metadata key='value' string.
+
+    Keys must begin with an ASCII alphabetic character and contain only
+    alphanumerics or underscore (_). Any illegal characters are replaced
+    with underscore*s* and an underscore is prepended if first character is a
+    digit; a UserWarning is raised if either of these changes are made.
+
+    Values are free (Unicode) text; the formatted string escapes any single
+    quotes (') with a backslash (\) and places single quotes around the value
+    (and around any escaped single quotes, e.g r"it's" -> r"'it'\''s'").
+
+    :param key: metadata key (must contain only alphanumerics and underscore)
+    :type key: str
+    :param val: metadata value (free text)
+    :type val: str
+    :returns: formatted string for output
+    :rtype: str
+    :raises UserWarning: if key is transformed to make it legal
+    """
+    good_key = key.encode().translate(LEGALIZE).decode()
+    if good_key[0].isdigit():
+        good_key = '_' + good_key
+    if good_key != key:
+        warnings.warn(UserWarning(
+            "mapped key '{0}' to '{1}".format(key, good_key)
+        ))
+    good_val = val.replace("'", r"'\''")
+    return "{0}='{1}'".format(str(good_key), str(good_val))
+
+
 def main():
-    """Print OpenStack metadata key=value pairs from configuration drive.
+    """Print OpenStack metadata key='value' pairs from configuration drive.
 
     :returns: int Exit code (1 on format error/file not found/access error)
     """
@@ -84,9 +121,9 @@ def main():
         meta_data = metadata(filename)
         if isinstance(meta_data[key], dict):
             for subkey in meta_data[key].keys():
-                print('{0}={1}'.format(subkey, meta_data[key][subkey]))
+                print(outformat(subkey, meta_data[key][subkey]))
         else:
-            print('{0}={1}'.format(key, meta_data[key]))
+            print(outformat(key, meta_data[key]))
         return 0
 
     except ValueError as err:
